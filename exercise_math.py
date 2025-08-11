8dd55b6f11b03e6573a38c3cc7759d7fa343a1fc
Maximo-Ottoni <mtottoni@mail.austral.edu.ar>
2025-08-11 12:44:50 -0300

    Add files via upload

--- exercise_math.py
+++ exercise_math.py
@@ -1,3 +1,18 @@
 def math():
     a = 57
     b = 7
+    print(a + b) #suma    
+    resta = a - b
+    print(resta) #resta
+    producto = (a * b)
+    print(producto)
+    promedio = (a + b)/2
+    print(promedio)
+    cociente_entero = (a // b)
+    print(cociente_entero)
+    resto_division = (a % b)
+    print(resto_division)
+    valor_real_division = (a / b)
+    print(valor_real_division)
+   
+math()
